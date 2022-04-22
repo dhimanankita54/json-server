e6c8daf88b64a699964c360c03310c0120cd1e33
dhimanankita54 <dhimanankita54@gmail.com>
2022-04-22 22:12:21 +0530

    idx

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-const command = `json-server --watch db.json --port ${port}`;
+// const command = `json-server --watch db.json --port ${port}`;
 
-exec(command, (err, stdout, stderr) => {
-  if (err) {
-    console.log("Error running exec", err);
-    return;
-  }
-  console.log("stdout:", stdout);
-  console.log("stderr:", stderr);
-});
+// exec(command, (err, stdout, stderr) => {
+//   if (err) {
+//     console.log("Error running exec", err);
+//     return;
+//   }
+//   console.log("stdout:", stdout);
+//   console.log("stderr:", stderr);
+// });
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
